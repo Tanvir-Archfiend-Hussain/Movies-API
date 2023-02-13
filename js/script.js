595f8c81015766de2a45e12d424892c088f920d0
archfiend-Tanvir <tanvirelhussain@gmail.com>
2023-02-13 22:58:08 +0000

    Script

--- js/script.js
+++ js/script.js
@@ -1,2 +1,10 @@
 //Initial Reference
-let movieNameRef = document.getElementById();
+let movieNameRef = document.getElementById("movie-name");
+let searchBtn = document.getElementById("search-btn");
+let result = document.getElementById("result");
+
+//Function to fetch data from API
+let getMovie = () => {
+  let movieName = movieNameRef.value;
+  let url = `http://www.omdbapi.com/?t=${movieName}&apikey=${}`;
+};
